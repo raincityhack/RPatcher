1fd1daf46f9c09b4a03b716b13f55c15a79ad50e
raincityhack <42696972+raincityhack@users.noreply.github.com>
2021-01-29 22:43:04 +0700

    Add files via upload

---
+++ daemon/INCLUDE/Offsets.h
@@ -0,0 +1,15 @@
+//
+// Created by PCAditiaID29 on 1/29/2021.
+//
+
+#ifndef RPATCHERDAEMON_OFFSETS_H
+#define RPATCHERDAEMON_OFFSETS_H
+
+namespace Offsets {
+    RADDR SmallCrosshair = 0x136DAB0;
+    RADDR LessRecoil = 0x136D4F8;
+    RADDR CharacterView = 0x381FCB0;
+    RADDR Aimbot100M = 0x258B740;
+}
+
+#endif //RPATCHERDAEMON_OFFSETS_H
